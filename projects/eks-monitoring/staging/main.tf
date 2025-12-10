52eaa7f93a2a27f41c41d492c25f01824b4424b9
oyegokeodev <oyegokeodev@gmail.com>
2025-12-10 04:44:44 +0100

    keep eks

--- projects/eks-monitoring/staging/main.tf
+++ projects/eks-monitoring/staging/main.tf
@@ -26,3 +26,22 @@ module "vpc" {
     "kubernetes.io/role/internal-elb" = "1"
   }
 }
+
+module "eks" {
+  source = "../../../modules/eks"
+
+  cluster_name    = "${var.project_name}-cluster"
+  cluster_version = "1.31"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnet_ids
+
+  # Allow public access to API server (so you can run kubectl from your laptop)
+  cluster_endpoint_public_access = true
+  
+  tags = {
+    Environment = "staging"
+    Owner       = "DevOps"
+    Project     = "eks-monitoring"
+  }
+}
--- projects/eks-monitoring/staging/outputs.tf
+++ projects/eks-monitoring/staging/outputs.tf
@@ -1,11 +1,29 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
 }
 
-output "private_subnets" {
-  value = module.vpc.private_subnet_ids
+output "private_subnet_ids" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnet_ids
 }
 
-output "public_subnets" {
-  value = module.vpc.public_subnet_ids
+output "public_subnet_ids" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
 }
