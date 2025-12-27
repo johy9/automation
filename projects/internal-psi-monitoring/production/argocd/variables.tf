db0954b03741fea20374cf320c158473763e7b22
oyegokeodev <oyegokeodev@gmail.com>
2025-12-27 21:43:54 +0100

    variables not used

--- projects/internal-psi-monitoring/production/argocd/variables.tf
+++ projects/internal-psi-monitoring/production/argocd/variables.tf
@@ -1,12 +1,3 @@
-variable "cluster_endpoint" {
-  description = "EKS cluster API endpoint"
-  type        = string
-}
-
-variable "cluster_ca_certificate" {
-  description = "EKS cluster CA certificate (base64 encoded)"
-  type        = string
-}
 variable "cluster_name" {
   description = "EKS cluster name"
   type        = string
