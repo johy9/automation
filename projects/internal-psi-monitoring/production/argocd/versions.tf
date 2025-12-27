398e5c5c517b020fe8205c85042384e72978a07a
oyegokeodev <oyegokeodev@gmail.com>
2025-12-27 22:32:58 +0100

    s3

--- projects/internal-psi-monitoring/production/argocd/versions.tf
+++ projects/internal-psi-monitoring/production/argocd/versions.tf
@@ -1,4 +1,14 @@
 terraform {
+
+  backend "s3" {
+    bucket = "oyegokeo-terraform-states"
+    # bucket       = "ringcentral-terraform-states"
+    key          = "internal-psi-monitoring/production/argocd/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+  
   required_version = ">= 1.11.0"
 
   required_providers {
