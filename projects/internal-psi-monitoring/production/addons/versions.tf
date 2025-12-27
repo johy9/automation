840628ff080362f22bdaeb15d636ff0c096b9ae1
oyegokeodev <oyegokeodev@gmail.com>
2025-12-27 20:03:56 +0100

    bucket correction

--- projects/internal-psi-monitoring/production/addons/versions.tf
+++ projects/internal-psi-monitoring/production/addons/versions.tf
@@ -21,7 +21,7 @@ terraform {
   required_version = ">= 1.11.0"
 
   backend "s3" {
-    bucket = "oyegokeodev-terraform-states"
+    bucket = "oyegokeo-terraform-states"
     # bucket       = "ringcentral-terraform-states"
     key          = "internal-psi-monitoring/production/addons/terraform.tfstate"
     region       = "us-east-1"
