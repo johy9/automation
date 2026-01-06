623ddf448ff922fcb514c1fe81727adc0820bb9b
johy9 <124749142+johy9@users.noreply.github.com>
2026-01-06 13:46:35 +0100

    Update main.tf

--- projects/internal-psi-monitoring/production/addons/main.tf
+++ projects/internal-psi-monitoring/production/addons/main.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "eks" {
   backend = "s3"
   config = {
     bucket = "oyegokeo-terraform-states"
-    key    = "internal-psi-monitoring/production/ekss/terraform.tfstate"
+    key    = "internal-psi-monitoring/production/eks/terraform.tfstate"
     region = "us-east-1"
   }
 }
